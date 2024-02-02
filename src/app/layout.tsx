dfaca71d3765719ef145091f6a193dc14aa0768a
damian <damianszczutkowski188@gmail.com>
2024-02-02 02:48:01 +0100

    v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 
-
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -18,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    das
+      das
       <body className={inter.className}>{children}</body>
     </html>
   )
